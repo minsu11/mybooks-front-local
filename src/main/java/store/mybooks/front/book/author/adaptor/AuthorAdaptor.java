e876ffd02af41da39ed7becf34d9ae683617ae2e
minsu <zmfjtnl1@naver.com>
2024-02-23 23:56:29 +0900

    feat: AuthorAdaptor 생성

---
+++ src/main/java/store/mybooks/front/book/author/adaptor/AuthorAdaptor.java
@@ -0,0 +1,15 @@
+package store.mybooks.front.book.author.adaptor;
+
+/**
+ * packageName    : store.mybooks.front.book.author.adaptor<br>
+ * fileName       : AuthorAdaptor<br>
+ * author         : minsu11<br>
+ * date           : 2/23/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/23/24        minsu11       최초 생성<br>
+ */
+public class AuthorAdaptor {
+}
