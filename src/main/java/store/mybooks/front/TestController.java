5ff3a5ccf48d5fe535eb076e469e4841b2febeb6
minsu <zmfjtnl1@naver.com>
2024-02-23 18:18:01 +0900

    feat: about-us layout 설정

---
+++ src/main/java/store/mybooks/front/TestController.java
@@ -0,0 +1,23 @@
+package store.mybooks.front;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * packageName    : store.mybooks.front<br>
+ * fileName       : TestController<br>
+ * author         : minsu11<br>
+ * date           : 2/23/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/23/24        minsu11       최초 생성<br>
+ */
+@Controller
+public class TestController {
+    @GetMapping("about-us")
+    public String test() {
+        return "about-us";
+    }
+}
