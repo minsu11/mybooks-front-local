1058120e6e62ae921f96bcb9fd9fbf9469197158
minsu <zmfjtnl1@naver.com>
2024-02-24 17:41:13 +0900

    feat: AuthorController 생성

--- src/main/java/store/mybooks/front/book/author/controller/AuthorController.java
+++ src/main/java/store/mybooks/front/book/author/controller/AuthorController.java
@@ -1,17 +1,6 @@
-package store.mybooks.front.book.author.controller;
-
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import store.mybooks.front.book.author.adaptor.AuthorAdaptor;
-import store.mybooks.front.book.author.dto.response.AuthorResponse;
-
-/**
- * packageName    : store.mybooks.front.book.author.controller<br>
- * fileName       : TestController<br>
+package store.mybooks.front.book.author.controller;/**
+ *packageName    : store.mybooks.front.book.author.controller<br>
+ * fileName       : AuthorController<br>
  * author         : minsu11<br>
  * date           : 2/24/24<br>
  * description    :
@@ -19,16 +8,5 @@ import store.mybooks.front.book.author.dto.response.AuthorResponse;
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
  * 2/24/24        minsu11       최초 생성<br>
- */
-@RestController
-@RequestMapping("test123")
-@RequiredArgsConstructor
-public class AuthorController {
-    private final AuthorAdaptor adaptor;
-
-    @GetMapping
-    public List<AuthorResponse> test(Pageable pageable) {
-        List<AuthorResponse> responses = adaptor.getAuthor().getContent();
-        return responses;
-    }
+ */public class AuthorController {
 }
