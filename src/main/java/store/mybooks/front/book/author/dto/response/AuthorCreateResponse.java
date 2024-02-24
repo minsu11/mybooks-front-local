c5432ddbd43c885be5affd1118cda8fc3dacaf52
minsu <zmfjtnl1@naver.com>
2024-02-24 16:51:24 +0900

    feat: AuthorCreateResponse 생성

---
+++ src/main/java/store/mybooks/front/book/author/dto/response/AuthorCreateResponse.java
@@ -0,0 +1,12 @@
+package store.mybooks.front.book.author.dto.response;/**
+ *packageName    : store.mybooks.front.book.author.dto.response<br>
+ * fileName       : AuthorCreateResponse<br>
+ * author         : minsu11<br>
+ * date           : 2/24/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/24/24        minsu11       최초 생성<br>
+ */public class AuthorCreateResponse {
+}
