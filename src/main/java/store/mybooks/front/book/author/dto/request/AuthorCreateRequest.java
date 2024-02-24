2ccf927efb32fc3368e23c1478ffdbbd0cb80023
minsu <zmfjtnl1@naver.com>
2024-02-24 16:47:53 +0900

    feat: service getAllAuthors 구현

---
+++ src/main/java/store/mybooks/front/book/author/dto/request/AuthorCreateRequest.java
@@ -0,0 +1,15 @@
+package store.mybooks.front.book.author.dto.request;
+
+/**
+ * packageName    : store.mybooks.front.book.author.dto.request<br>
+ * fileName       : AuthorCreateRequest<br>
+ * author         : minsu11<br>
+ * date           : 2/24/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/24/24        minsu11       최초 생성<br>
+ */
+public class AuthorCreateRequest {
+}
