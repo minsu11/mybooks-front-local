b3e0f34db9596373367cc880bf0df36ed6509558
minsu <zmfjtnl1@naver.com>
2024-02-26 00:30:54 +0900

    feat: registerPublisher 구현

--- src/main/java/store/mybooks/front/book/publisher/adaptor/PublisherAdaptor.java
+++ src/main/java/store/mybooks/front/book/publisher/adaptor/PublisherAdaptor.java
@@ -1,12 +1,13 @@
 package store.mybooks.front.book.publisher.adaptor;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import store.mybooks.front.book.publisher.dto.request.PublisherCreateRequest;
+import store.mybooks.front.book.publisher.dto.response.PublisherCreateResponse;
 import store.mybooks.front.book.publisher.dto.response.PublisherResponse;
 import store.mybooks.front.config.GatewayAdaptorProperties;
 import store.mybooks.front.pageable.dto.response.PageResponse;
@@ -47,4 +48,32 @@ public class PublisherAdaptor {
         }
         return exchange.getBody();
     }
+
+    /**
+     * methodName : registerPublisher<br>
+     * author : minsu11<br>
+     * description : 등록할 출판사 정보를 백엔드에 보낸 뒤 정상 등록이 된다면 응답 정보를 보냄
+     * <br> *
+     *
+     * @param publisherCreateRequest 등록할 출판사 DTO
+     * @return publisher create response
+     * @throws RuntimeException {@code http status code created}가 아니면 예외를 던짐
+     */
+    public PublisherCreateResponse registerPublisher(PublisherCreateRequest publisherCreateRequest) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<PublisherCreateRequest> request = new HttpEntity<>(publisherCreateRequest, headers);
+        ResponseEntity<PublisherCreateResponse> exchange = restTemplate.exchange(gatewayAdaptorProperties.getAddress() + "/api/publishers",
+                HttpMethod.POST,
+                request,
+                new ParameterizedTypeReference<PublisherCreateResponse>() {
+                });
+        if (exchange.getStatusCode() != HttpStatus.CREATED) {
+            throw new RuntimeException();
+        }
+        return exchange.getBody();
+    }
+
 }
