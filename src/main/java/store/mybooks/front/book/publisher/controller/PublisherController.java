96061fa56800516c4a3000b974e3026e1e428c53
minsu <zmfjtnl1@naver.com>
2024-02-25 23:32:41 +0900

    feat: publisherController 생성

---
+++ src/main/java/store/mybooks/front/book/publisher/controller/PublisherController.java
@@ -0,0 +1,15 @@
+package store.mybooks.front.book.publisher.controller;
+
+/**
+ * packageName    : store.mybooks.front.book.publisher.controller<br>
+ * fileName       : PublisherController<br>
+ * author         : minsu11<br>
+ * date           : 2/25/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/25/24        minsu11       최초 생성<br>
+ */
+public class PublisherController {
+}
