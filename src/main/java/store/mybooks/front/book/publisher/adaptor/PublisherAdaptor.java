060a3a671f1bb38a4c4847bfc365dd9ebe335a1c
minsu <zmfjtnl1@naver.com>
2024-02-25 23:36:19 +0900

    feat: PublisherAdaptor 생성

---
+++ src/main/java/store/mybooks/front/book/publisher/adaptor/PublisherAdaptor.java
@@ -0,0 +1,15 @@
+package store.mybooks.front.book.publisher.adaptor;
+
+/**
+ * packageName    : store.mybooks.front.book.publisher.adaptor<br>
+ * fileName       : PublisherAdaptor<br>
+ * author         : minsu11<br>
+ * date           : 2/25/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/25/24        minsu11       최초 생성<br>
+ */
+public class PublisherAdaptor {
+}
