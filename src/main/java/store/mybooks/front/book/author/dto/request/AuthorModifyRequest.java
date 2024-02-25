ee0a0b1e08c744019fcbbab36f0340e59416ae8d
minsu <zmfjtnl1@naver.com>
2024-02-25 17:22:19 +0900

    feat: AuthorModifyRequest 구현

--- src/main/java/store/mybooks/front/book/author/dto/request/AuthorModifyRequest.java
+++ src/main/java/store/mybooks/front/book/author/dto/request/AuthorModifyRequest.java
@@ -1,15 +1,23 @@
 package store.mybooks.front.book.author.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * packageName    : store.mybooks.front.book.author.dto.request<br>
  * fileName       : AuthorModifyRequest<br>
  * author         : minsu11<br>
  * date           : 2/25/24<br>
- * description    :
+ * description    : 수정할 저자의 정보를 가지고 있는 DTO
  * ===========================================================<br>
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
  * 2/25/24        minsu11       최초 생성<br>
  */
+@Data
+@AllArgsConstructor
 public class AuthorModifyRequest {
+    private String name;
+    private String content;
+
 }
