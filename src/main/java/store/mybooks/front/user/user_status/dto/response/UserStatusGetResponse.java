d3f2858af81e64a908a0c9fd262f6947fbfb6215
masiljangajji <hjk172262@gmail.com>
2024-02-25 10:42:07 +0900

    feat: UserStatusGetResponse 추가

---
+++ src/main/java/store/mybooks/front/user/user_status/dto/response/UserStatusGetResponse.java
@@ -0,0 +1,21 @@
+package store.mybooks.front.user.user_status.dto.response;
+
+import lombok.Getter;
+
+/**
+ * packageName    : store.mybooks.resource.user_status.dto.response
+ * fileName       : UserStatusGetResponse
+ * author         : masiljangajji
+ * date           : 2/19/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/19/24        masiljangajji       최초 생성
+ */
+@Getter
+public class UserStatusGetResponse {
+
+    String id;
+
+}
