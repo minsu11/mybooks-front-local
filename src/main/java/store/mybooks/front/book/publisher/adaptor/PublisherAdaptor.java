516fbbe88cb05a639ea805f08f0bed2c0d84c888
minsu <zmfjtnl1@naver.com>
2024-02-25 23:44:23 +0900

    feat: PublisherAdaptor getPublisherList 구현

--- src/main/java/store/mybooks/front/book/publisher/adaptor/PublisherAdaptor.java
+++ src/main/java/store/mybooks/front/book/publisher/adaptor/PublisherAdaptor.java
@@ -1,5 +1,16 @@
 package store.mybooks.front.book.publisher.adaptor;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import store.mybooks.front.book.publisher.dto.response.PublisherResponse;
+import store.mybooks.front.config.GatewayAdaptorProperties;
+import store.mybooks.front.pageable.dto.response.PageResponse;
+
 /**
  * packageName    : store.mybooks.front.book.publisher.adaptor<br>
  * fileName       : PublisherAdaptor<br>
@@ -11,5 +22,21 @@ package store.mybooks.front.book.publisher.adaptor;
  * -----------------------------------------------------------<br>
  * 2/25/24        minsu11       최초 생성<br>
  */
+@Component
+@RequiredArgsConstructor
 public class PublisherAdaptor {
+    private RestTemplate restTemplate;
+    private GatewayAdaptorProperties gatewayAdaptorProperties;
+
+    public PageResponse<PublisherResponse> getPublisherList() {
+        ResponseEntity<PageResponse<PublisherResponse>> exchange = restTemplate.exchange(gatewayAdaptorProperties.getAddress() + "/api/publishers",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<PageResponse<PublisherResponse>>() {
+                });
+        if (exchange.getStatusCode() != HttpStatus.OK) {
+            throw new RuntimeException();
+        }
+        return exchange.getBody();
+    }
 }
--- src/main/java/store/mybooks/front/book/publisher/controller/PublisherController.java
+++ src/main/java/store/mybooks/front/book/publisher/controller/PublisherController.java
@@ -1,5 +1,9 @@
 package store.mybooks.front.book.publisher.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * packageName    : store.mybooks.front.book.publisher.controller<br>
  * fileName       : PublisherController<br>
@@ -11,5 +15,9 @@ package store.mybooks.front.book.publisher.controller;
  * -----------------------------------------------------------<br>
  * 2/25/24        minsu11       최초 생성<br>
  */
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/publisher")
 public class PublisherController {
+
 }
