daf1f5571f2c046f5e8a60e13cbee680fbca4cc6
minsu <zmfjtnl1@naver.com>
2024-02-26 00:16:23 +0900

    feat: 출판사 dto 구현

---
+++ src/main/java/store/mybooks/front/book/publisher/dto/request/PublisherCreateRequest.java
@@ -0,0 +1,23 @@
+package store.mybooks.front.book.publisher.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : store.mybooks.front.book.publisher.dto.request<br>
+ * fileName       : PublisherCreateRequest<br>
+ * author         : minsu11<br>
+ * date           : 2/26/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/26/24        minsu11       최초 생성<br>
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PublisherCreateRequest {
+    private String name;
+}
