b36038c00acdf00c982355212a1aefdf0dd5293f
minsu <zmfjtnl1@naver.com>
2024-02-25 21:05:11 +0900

    feat: AuthorDeleteResponse 생성

---
+++ src/main/java/store/mybooks/front/book/author/dto/response/AuthorDeleteResponse.java
@@ -0,0 +1,12 @@
+package store.mybooks.front.book.author.dto.response;/**
+ *packageName    : store.mybooks.front.book.author.dto.response<br>
+ * fileName       : AuthorDeleteResponse<br>
+ * author         : minsu11<br>
+ * date           : 2/25/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/25/24        minsu11       최초 생성<br>
+ */public class AuthorDeleteResponse {
+}
