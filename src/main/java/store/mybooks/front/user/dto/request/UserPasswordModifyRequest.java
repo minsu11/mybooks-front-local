9802206cfd297b95b16be2893ca3f94ac20961d7
masiljangajji <hjk172262@gmail.com>
2024-02-26 09:37:04 +0900

    feat: UserModify Grade,Password,Status Request 추가

---
+++ src/main/java/store/mybooks/front/user/dto/request/UserPasswordModifyRequest.java
@@ -0,0 +1,24 @@
+package store.mybooks.front.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : store.mybooks.resource.user.dto.request
+ * fileName       : UserPasswordModifyRequest
+ * author         : masiljangajji
+ * date           : 2/25/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/25/24        masiljangajji       최초 생성
+ */
+@Getter
+@AllArgsConstructor
+public class UserPasswordModifyRequest {
+
+    private String password;
+
+}
---
+++ src/main/java/store/mybooks/front/user/dto/request/UserStatusModifyRequest.java
@@ -0,0 +1,23 @@
+package store.mybooks.front.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : store.mybooks.resource.user.dto.request
+ * fileName       : UserStatusModifyRequest
+ * author         : masiljangajji
+ * date           : 2/25/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/25/24        masiljangajji       최초 생성
+ */
+@Getter
+@AllArgsConstructor
+public class UserStatusModifyRequest {
+    private String userStatusName;
+
+}
