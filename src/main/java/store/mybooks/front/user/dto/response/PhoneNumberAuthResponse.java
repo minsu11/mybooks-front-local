2a3e2f37e27680b457d99b97491cb118dab89112
masiljangajji <hjk172262@gmail.com>
2024-02-26 10:09:11 +0900

    feat; PhoneNumberAuthResponse 추가

---
+++ src/main/java/store/mybooks/front/user/dto/response/PhoneNumberAuthResponse.java
@@ -0,0 +1,24 @@
+package store.mybooks.front.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * packageName    : store.mybooks.authorization.dooray.dto.response
+ * fileName       : PhoneNumberAuthResponse
+ * author         : masiljangajji
+ * date           : 2/26/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/26/24        masiljangajji       최초 생성
+ */
+
+@Getter
+@AllArgsConstructor
+public class PhoneNumberAuthResponse {
+
+    private final String randomNumber;
+
+}
