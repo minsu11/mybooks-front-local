f86d1f2ed7de676ec581c5fb49cf0d5b940af9c1
damho <ydh4745@gamil.com>
2024-02-26 17:41:57 +0900

    refactor: CategoryIdAndName.java 작성. 카테고리 아이디, 이름만 가지고 있는 DTO

---
+++ src/main/java/store/mybooks/front/admin/category/model/response/CategoryIdAndName.java
@@ -0,0 +1,24 @@
+package store.mybooks.front.admin.category.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * packageName    : store.mybooks.front.category.model
+ * fileName       : CategoryIdAndName
+ * author         : damho-lee
+ * date           : 2/24/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/24/24          damho-lee          최초 생성
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class CategoryIdAndName {
+    private Integer id;
+    private String name;
+}
