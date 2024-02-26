9fdaeb5af412075bdc9aa8c86da4bc3d9ea5d899
masiljangajji <hjk172262@gmail.com>
2024-02-26 10:04:41 +0900

    feat: UserAdaptor modify 세분화 (이름+전화번호) , 비밀번호 , 등급 , 상태

--- src/main/java/store/mybooks/front/user/adaptor/UserAdaptor.java
+++ src/main/java/store/mybooks/front/user/adaptor/UserAdaptor.java
@@ -11,13 +11,21 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import store.mybooks.front.book_order.model.Response;
 import store.mybooks.front.config.GatewayAdaptorProperties;
 import store.mybooks.front.user.dto.request.UserCreateRequest;
+import store.mybooks.front.user.dto.request.UserGradeModifyRequest;
 import store.mybooks.front.user.dto.request.UserLoginRequest;
+import store.mybooks.front.user.dto.request.UserModifyRequest;
+import store.mybooks.front.user.dto.request.UserPasswordModifyRequest;
+import store.mybooks.front.user.dto.request.UserStatusModifyRequest;
+import store.mybooks.front.user.dto.response.PhoneNumberAuthResponse;
 import store.mybooks.front.user.dto.response.UserCreateResponse;
+import store.mybooks.front.user.dto.response.UserGetResponse;
+import store.mybooks.front.user.dto.response.UserGradeModifyResponse;
 import store.mybooks.front.user.dto.response.UserLoginResponse;
-import store.mybooks.front.user_status.dto.response.UserStatusGetResponse;
+import store.mybooks.front.user.dto.response.UserModifyResponse;
+import store.mybooks.front.user.dto.response.UserPasswordModifyResponse;
+import store.mybooks.front.user.dto.response.UserStatusModifyResponse;
 
 /**
  * packageName    : store.mybooks.front.user.adaptor
@@ -39,47 +47,93 @@ public class UserAdaptor {
 
     private final GatewayAdaptorProperties gatewayAdaptorProperties;
 
-    public UserLoginResponse loginUser(UserLoginRequest userLoginRequest) {
+
+
+
+
+
+
+
+    public void modifyUserPassword(Long userId, UserPasswordModifyRequest modifyRequest){
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
 
-        HttpEntity<UserLoginRequest> requestEntity = new HttpEntity<>(userLoginRequest, headers);
+        HttpEntity<UserPasswordModifyRequest> requestEntity = new HttpEntity<>(modifyRequest, headers);
 
-
-        ResponseEntity<UserLoginResponse> responseEntity =
-                restTemplate.exchange(gatewayAdaptorProperties.getAddress() + "/api/users/login", HttpMethod.POST,
+        ResponseEntity<UserPasswordModifyResponse> responseEntity =
+                restTemplate.exchange(gatewayAdaptorProperties.getAddress() + "/api/users/{userId}/password", HttpMethod.PUT,
                         requestEntity,
                         new ParameterizedTypeReference<>() {
-                        });
+                        },userId);
+
 
         if (responseEntity.getStatusCode() != HttpStatus.OK) {
             throw new RuntimeException();
         }
-        return responseEntity.getBody();
     }
 
-    public UserCreateResponse createUser(UserCreateRequest createRequest) {
+    public void modifyUserStatus(Long userId, UserStatusModifyRequest modifyRequest){
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
 
-        HttpEntity<UserCreateRequest> requestEntity = new HttpEntity<>(createRequest, headers);
+        HttpEntity<UserStatusModifyRequest> requestEntity = new HttpEntity<>(modifyRequest, headers);
 
-        ResponseEntity<UserCreateResponse> responseEntity =
-                restTemplate.exchange(gatewayAdaptorProperties.getAddress() + "/api/users", HttpMethod.POST,
+        ResponseEntity<UserStatusModifyResponse> responseEntity =
+                restTemplate.exchange(gatewayAdaptorProperties.getAddress() + "/api/users/{userId}/status", HttpMethod.PUT,
                         requestEntity,
                         new ParameterizedTypeReference<>() {
-                        });
+                        },userId);
 
 
-        if (responseEntity.getStatusCode() != HttpStatus.CREATED) {
+        if (responseEntity.getStatusCode() != HttpStatus.OK) {
             throw new RuntimeException();
         }
-        return responseEntity.getBody();
     }
+
+    public void modifyUserGrade(Long userId, UserGradeModifyRequest modifyRequest){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<UserGradeModifyRequest> requestEntity = new HttpEntity<>(modifyRequest, headers);
+
+        ResponseEntity<UserGradeModifyResponse> responseEntity =
+                restTemplate.exchange(gatewayAdaptorProperties.getAddress() + "/api/users/{userId}/grade", HttpMethod.PUT,
+                        requestEntity,
+                        new ParameterizedTypeReference<>() {
+                        },userId);
+
+
+        if (responseEntity.getStatusCode() != HttpStatus.OK) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void modifyUser(Long userId, UserModifyRequest modifyRequest){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<UserModifyRequest> requestEntity = new HttpEntity<>(modifyRequest, headers);
+
+        ResponseEntity<UserModifyResponse> responseEntity =
+                restTemplate.exchange(gatewayAdaptorProperties.getAddress() + "/api/users/{userId}", HttpMethod.PUT,
+                        requestEntity,
+                        new ParameterizedTypeReference<>() {
+                        },userId);
+
+
+        if (responseEntity.getStatusCode() != HttpStatus.OK) {
+            throw new RuntimeException();
+        }
+    }
+
 
 
 }
