e23e3e3d6c699d46d46ee16e99d2afdb19dda73f
damho <ydh4745@gamil.com>
2024-02-26 17:44:41 +0900

    feat: TagAdaptor.java 작성

---
+++ src/main/java/store/mybooks/front/admin/category/adaptor/TagAdaptor.java
@@ -0,0 +1,22 @@
+package store.mybooks.front.admin.category.adaptor;
+
+import store.mybooks.front.admin.category.model.request.TagCreateRequest;
+import store.mybooks.front.admin.category.model.response.TagGetResponse;
+import store.mybooks.front.pageable.dto.response.PageResponse;
+
+/**
+ * packageName    : store.mybooks.front.category.adaptor
+ * fileName       : TagAdaptor
+ * author         : damho-lee
+ * date           : 2/26/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/26/24          damho-lee          최초 생성
+ */
+public interface TagAdaptor {
+    PageResponse<TagGetResponse> getTags(int page, int size);
+
+    void createTag(TagCreateRequest tagCreateRequest);
+}
