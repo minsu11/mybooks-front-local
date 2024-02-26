1a2cbf4c5cdf35200f3a8b87dd91529875242392
damho <ydh4745@gamil.com>
2024-02-26 17:41:05 +0900

    refactor: CategoryGetResponseForUpdate.java 작성. 수정페이지에서 기본 값을 보여주기 위한 Dto.

---
+++ src/main/java/store/mybooks/front/admin/category/model/response/CategoryGetResponseForUpdate.java
@@ -0,0 +1,27 @@
+package store.mybooks.front.admin.category.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * packageName    : store.mybooks.front.category.model.response
+ * fileName       : CategoryGetResponseForUpdate
+ * author         : damho-lee
+ * date           : 2/26/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/26/24          damho-lee          최초 생성
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryGetResponseForUpdate {
+    private CategoryGetResponse targetCategory;
+    private CategoryGetResponse levelOneCategory;
+    private CategoryGetResponse levelTwoCategory;
+}
