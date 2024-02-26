b173d55ad291b23f1f4da558faf38a0aa5384430
damho <ydh4745@gamil.com>
2024-02-26 17:42:56 +0900

    feat: CategoryModifyRequest.java 작성.

---
+++ src/main/java/store/mybooks/front/admin/category/model/request/CategoryModifyRequest.java
@@ -0,0 +1,26 @@
+package store.mybooks.front.admin.category.model.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * packageName    : store.mybooks.front.category.model.request
+ * fileName       : CategoryModifyRequest
+ * author         : damho-lee
+ * date           : 2/26/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/26/24          damho-lee          최초 생성
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryModifyRequest {
+    private Integer id;
+    private Integer firstParentCategoryId;
+    private Integer secondParentCategoryId;
+    private String name;
+}
