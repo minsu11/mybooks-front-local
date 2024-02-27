1b6f52f2b4b37fd4e0fbcc25cfe6311a02eb45bb
damho <ydh4745@gamil.com>
2024-02-27 20:45:21 +0900

    refactor: 필요없는 코드 삭제

--- src/main/resources/templates/admin/js/tag/tag-register-validation.js
+++ src/main/resources/templates/admin/js/tag/tag-register-validation.js
@@ -3,8 +3,6 @@ var form = document.forms["form"];
 form.addEventListener("submit", function (event) {
     var name = form.name.value.trim();
 
-    alert(name);
-
     if (!validationForm(name)) {
         event.preventDefault();
     }
