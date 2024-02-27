42b1df5b57b39cbdd4991928e4cf29f457315e3f
newjaehun <sjh030798@gmail.com>
2024-02-27 16:36:19 +0900

    feat: Admin/book Model 추가

---
+++ src/main/java/store/mybooks/front/admin/book/model/request/BookCreateRequest.java
@@ -0,0 +1,65 @@
+package store.mybooks.front.admin.book.model.request;
+
+import java.time.LocalDate;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ *packageName    : store.mybooks.front.admin.book.model.request <br/>
+ * fileName       : BookCreateRequest<br/>
+ * author         : newjaehun <br/>
+ * date           : 2/26/24<br/>
+ * description    :<br/>
+ * ===========================================================<br/>
+ * DATE              AUTHOR             NOTE<br/>
+ * -----------------------------------------------------------<br/>
+ * 2/26/24        newjaehun       최초 생성<br/>
+ */
+@Getter
+@AllArgsConstructor
+public class BookCreateRequest {
+    @NotBlank
+    @Size(min = 1, max = 20)
+    private String bookStatusId;
+    @NotNull
+    @Min(1)
+    @Max(100)
+    private Integer publisherId;
+    @NotBlank
+    @Size(min = 1, max = 100)
+    private String name;
+    @NotBlank
+    @Size(min = 13, max = 13)
+    private String isbn;
+    @NotNull
+    @Past
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate publishDate;
+    @NotNull
+    @Positive
+    private Integer page;
+    @NotBlank
+    private String index;
+    @NotBlank
+    private String content;
+    @NotNull
+    @Positive
+    private Integer originalCost;
+    @NotNull
+    @Positive
+    private Integer saleCost;
+    @NotNull
+    @PositiveOrZero
+    private Integer stock;
+    @NotNull
+    private Boolean isPacking;
+}
