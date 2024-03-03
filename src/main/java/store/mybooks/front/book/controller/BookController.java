2966527fedf2824eb740c0e19ce34284ed6ea264
newjaehun <sjh030798@gmail.com>
2024-03-03 15:39:54 +0900

    refactor: BookController 수정

--- src/main/java/store/mybooks/front/book/controller/BookController.java
+++ src/main/java/store/mybooks/front/book/controller/BookController.java
@@ -1,15 +1,15 @@
 package store.mybooks.front.book.controller;
 
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import store.mybooks.front.admin.author.service.AuthorService;
-import store.mybooks.front.admin.category.service.CategoryService;
-import store.mybooks.front.admin.publisher.service.PublisherService;
-import store.mybooks.front.admin.tag.service.TagService;
+import store.mybooks.front.admin.author.dto.response.AuthorGetResponse;
+import store.mybooks.front.admin.book.model.response.BookDetailResponse;
+import store.mybooks.front.admin.tag.model.response.TagGetResponseForBookDetail;
 import store.mybooks.front.book.service.BookService;
 
 /**
@@ -28,16 +28,20 @@ import store.mybooks.front.book.service.BookService;
 @RequestMapping("/book")
 public class BookController {
     private final BookService bookService;
-    private final PublisherService publisherService;
-    private final CategoryService categoryService;
-    private final TagService tagService;
-    private final AuthorService authorService;
+
 
     @GetMapping("/{id}")
     public String getBookDetailPage(@PathVariable("id") Long bookId, Model model) {
-        model.addAttribute("book", bookService.getBook(bookId));
-//        model.addAttribute("categoryList", )
+        BookDetailResponse book = bookService.getBook(bookId);
+        model.addAttribute("book", book);
+        model.addAttribute("authorNameList", book.getAuthorList().stream()
+                .map(AuthorGetResponse::getName)
+                .collect(Collectors.joining(", ")));
+        model.addAttribute("tagNameList", book.getTagList().stream()
+                .map(TagGetResponseForBookDetail::getName)
+                .collect(Collectors.joining(", ")));
 
+//        model.addAttribute("categoryList", )
         return "book-details";
     }
 
