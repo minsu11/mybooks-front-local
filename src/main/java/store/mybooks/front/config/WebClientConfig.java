84715f630449a12b83b8683509640b4c21a140c5
masiljangajji <hjk172262@gmail.com>
2024-03-05 21:39:03 +0900

    feat: WebClientConfig  CookieInterceptor 설정 추가

--- src/main/java/store/mybooks/front/config/WebClientConfig.java
+++ src/main/java/store/mybooks/front/config/WebClientConfig.java
@@ -5,6 +5,9 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import store.mybooks.front.utils.interceptor.CookieInterceptor;
 
 /**
  * packageName    : store.mybooks.front.config
@@ -18,12 +21,20 @@ import org.springframework.web.client.RestTemplate;
  * 2/19/24        minsu11       최초 생성
  */
 @Configuration
-public class WebClientConfig {
+public class WebClientConfig implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder
                 .setConnectTimeout(Duration.ofSeconds(3L))
                 .setReadTimeout(Duration.ofSeconds(3L))
                 .build();
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CookieInterceptor())
+                .addPathPatterns("/user/**")
+                .addPathPatterns("/admin/**")
+                .addPathPatterns("/book/**")
+                .addPathPatterns("/login","/");
     }
 }
