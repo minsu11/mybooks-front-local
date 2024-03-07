87fd67c9211fd8722ec580a95b8822b4f570d556
masiljangajji <hjk172262@gmail.com>
2024-03-07 14:58:57 +0900

    refactor: Exception Handler 로직변경

--- src/main/java/store/mybooks/front/global/GlobalControllerAdvice.java
+++ src/main/java/store/mybooks/front/global/GlobalControllerAdvice.java
@@ -1,16 +1,13 @@
 package store.mybooks.front.global;
 
-import org.springframework.ui.Model;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import store.mybooks.front.auth.exception.AccessIdForbiddenException;
 import store.mybooks.front.auth.exception.AuthenticationIsNotValidException;
 import store.mybooks.front.auth.exception.StatusIsNotActiveException;
-import store.mybooks.front.global.exception.ManageFailedException;
-import store.mybooks.front.global.exception.ValidationFailException;
 
 /**
  * packageName    : store.mybooks.front.global
@@ -25,29 +22,23 @@ import store.mybooks.front.global.exception.ValidationFailException;
  */
 @ControllerAdvice
 public class GlobalControllerAdvice {
+
+    private static final String REFERER = "referer";
+
     /**
      * methodName : badRequestException <br>
      * author : damho-lee <br>
      * description : resource 서버에서 BadRequest, NotFound 상태코드가 오는 경우를 처리하는 ExceptionHandler.<br>
      *
      * @param exception HttpClientErrorException
-     * @param model     Model
      * @return string
      */
-    @ExceptionHandler({HttpClientErrorException.BadRequest.class, HttpClientErrorException.NotFound.class,
-            ValidationFailException.class})
-    public String badRequestException(Exception exception, Model model) {
-        if (exception.getMessage() != null) {
-            model.addAttribute("message", exception.getMessage());
-        }
-        return "admin/view/error";
-    }
+    @ExceptionHandler({HttpClientErrorException.BadRequest.class, HttpClientErrorException.NotFound.class})
+    // 400 404 이게 리소스에서 나오는 모든 예외
+    public ModelAndView handleBadRequestAndNotFoundException(Exception exception, HttpServletRequest request) {
 
-    @ExceptionHandler({ManageFailedException.class})
-    public ModelAndView registerFailedException(ManageFailedException exception,
-                                                RedirectAttributes redirectAttributes) {
-        ModelAndView modelAndView = new ModelAndView(exception.getUrl());
-        modelAndView.addObject("msg", exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView(request.getHeader(REFERER));
+        modelAndView.addObject("error", exception.getMessage());
         return modelAndView;
     }
 
@@ -56,8 +47,9 @@ public class GlobalControllerAdvice {
     public String handleAuthException(RuntimeException ex) {
 
         if (ex instanceof AuthenticationIsNotValidException) {
-            return "redirect:/login"; // 다시 로그인
-        }else if(ex instanceof StatusIsNotActiveException){
+            return "redirect:/login"; // 토큰조작 다시 로그인
+        } else if (ex instanceof StatusIsNotActiveException) {
+            // 유저가 탈퇴했음
             return "redirect:/dormancy"; // todo 휴대폰인증 페이지로
         }
 
