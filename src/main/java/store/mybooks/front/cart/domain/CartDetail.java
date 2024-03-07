d5a014b7e87b7199558d950e43a9638bf0a7fd44
hyeonjaez <jhjh0022@naver.com>
2024-03-07 10:13:31 +0900

    feat: cartdetail dto

---
+++ src/main/java/store/mybooks/front/cart/domain/CartDetail.java
@@ -0,0 +1,37 @@
+package store.mybooks.front.cart.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * packageName    : store.mybooks.front.cart <br/>
+ * fileName       : CartDetail<br/>
+ * author         : Fiat_lux <br/>
+ * date           : 3/3/24<br/>
+ * description    :<br/>
+ * ===========================================================<br/>
+ * DATE              AUTHOR             NOTE<br/>
+ * -----------------------------------------------------------<br/>
+ * 3/3/24        Fiat_lux       최초 생성<br/>
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartDetail {
+    private Long bookId;
+    private int cartDetailAmount;
+    private String name;
+    private String bookImage;
+    private String saleCost;
+
+    public void amountUpdate(int amount) {
+        this.cartDetailAmount = this.cartDetailAmount + amount;
+    }
+
+}
