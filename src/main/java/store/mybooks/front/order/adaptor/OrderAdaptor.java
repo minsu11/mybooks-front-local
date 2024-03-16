885e6ce13948e2fcb2f4c9cb7bee543251bc7e37
minsu <zmfjtnl1@naver.com>
2024-03-16 15:22:13 +0900

    feat: 주문 생성 dto, 주문 생성 adaptor 파일 생성

---
+++ src/main/java/store/mybooks/front/order/adaptor/OrderAdaptor.java
@@ -0,0 +1,12 @@
+package store.mybooks.front.order.adaptor;/**
+ *packageName    : store.mybooks.front.order.adaptor<br>
+ * fileName       : OrderAdaptor<br>
+ * author         : minsu11<br>
+ * date           : 3/16/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 3/16/24        minsu11       최초 생성<br>
+ */public class OrderAdaptor {
+}
---
+++ src/main/java/store/mybooks/front/order/dto/request/OrderCreateRequest.java
@@ -0,0 +1,12 @@
+package store.mybooks.front.order.dto.request;/**
+ *packageName    : store.mybooks.front.order.dto.request<br>
+ * fileName       : OrderCreateRequest<br>
+ * author         : minsu11<br>
+ * date           : 3/16/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 3/16/24        minsu11       최초 생성<br>
+ */public class OrderCreateRequest {
+}
