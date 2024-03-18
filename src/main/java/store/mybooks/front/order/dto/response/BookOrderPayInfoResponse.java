b86931b1484fec99583842da7a27ea95b346e96c
minsu <zmfjtnl1@naver.com>
2024-03-19 03:35:21 +0900

    feat: 결제 시 필요한 정보 담긴 dto

---
+++ src/main/java/store/mybooks/front/order/dto/response/BookOrderPayInfoResponse.java
@@ -0,0 +1,12 @@
+package store.mybooks.front.order.dto.response;/**
+ *packageName    : store.mybooks.front.order.dto.response<br>
+ * fileName       : BookOrderPayInfoResponse<br>
+ * author         : minsu11<br>
+ * date           : 3/19/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 3/19/24        minsu11       최초 생성<br>
+ */public class BookOrderPayInfoResponse {
+}
