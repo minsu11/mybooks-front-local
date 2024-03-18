17636f27b00cf62ea2c95daed3a3daaaf3888eb2
minsu <zmfjtnl1@naver.com>
2024-03-19 03:35:53 +0900

    feat: 어노테이션 추가

--- src/main/java/store/mybooks/front/order/dto/response/BookOrderPayInfoResponse.java
+++ src/main/java/store/mybooks/front/order/dto/response/BookOrderPayInfoResponse.java
@@ -1,5 +1,10 @@
-package store.mybooks.front.order.dto.response;/**
- *packageName    : store.mybooks.front.order.dto.response<br>
+package store.mybooks.front.order.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * packageName    : store.mybooks.front.order.dto.response<br>
  * fileName       : BookOrderPayInfoResponse<br>
  * author         : minsu11<br>
  * date           : 3/19/24<br>
@@ -8,5 +13,14 @@ package store.mybooks.front.order.dto.response;/**
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
  * 3/19/24        minsu11       최초 생성<br>
- */public class BookOrderPayInfoResponse {
+ */
+@Getter
+@AllArgsConstructor
+public class BookOrderPayInfoResponse {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String orderNumber;
+    private String orderName;
+    private String orderStatus;
 }
