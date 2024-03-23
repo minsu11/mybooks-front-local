dc343b9a6b3ea108c6229d79c622a08c96df1ec5
hyeonjaez <jhjh0022@naver.com>
2024-03-23 14:25:01 +0900

    feat: book controller

--- src/main/java/store/mybooks/front/home/HomeController.java
+++ src/main/java/store/mybooks/front/home/HomeController.java
@@ -1,12 +1,19 @@
 package store.mybooks.front.home;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import store.mybooks.front.admin.book.model.response.BookLikeResponse;
+import store.mybooks.front.admin.book.model.response.BookPopularityResponse;
+import store.mybooks.front.admin.book.model.response.BookPublicationDateResponse;
+import store.mybooks.front.admin.book.model.response.BookRatingResponse;
+import store.mybooks.front.admin.book.model.response.BookReviewResponse;
 import store.mybooks.front.admin.book.service.BookAdminService;
+import store.mybooks.front.book.service.BookService;
 
 /**
  * packageName    : store.mybooks.front
@@ -25,11 +32,23 @@ import store.mybooks.front.admin.book.service.BookAdminService;
 public class HomeController {
 
     private final BookAdminService bookAdminService;
-
+    private final BookService bookService;
 
     @RequestMapping("/")
     public String home(@PageableDefault(size = 8) Pageable pageable, Model model) {
-        model.addAttribute("books", bookAdminService.getBooks(pageable));
+
+        List<BookPopularityResponse> bookPopularitySortList = bookService.getBookPopularitySortList();
+        List<BookLikeResponse> bookLikeSortList = bookService.getBookLikeSortList();
+        List<BookReviewResponse> bookReviewSortList = bookService.getBookReviewSortList();
+        List<BookRatingResponse> bookRatingSortList = bookService.getBookRatingSortList();
+        List<BookPublicationDateResponse> bookPublicationDateSortList = bookService.getBookPublicationDateSortList();
+
+        model.addAttribute("popularList", bookPopularitySortList);
+        model.addAttribute("likeList", bookLikeSortList);
+        model.addAttribute("reviewList", bookReviewSortList);
+        model.addAttribute("ratingList", bookRatingSortList);
+        model.addAttribute("publicationList", bookPublicationDateSortList);
+//        model.addAttribute("books", bookAdminService.getBooks(pageable));
         return "index";
     }
 
