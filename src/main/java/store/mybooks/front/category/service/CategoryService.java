79dff53a29c16425b6fda295e9054c6e79316e4d
damho <ydh4745@gamil.com>
2024-03-24 20:46:09 +0900

    refactor: db 에서 카테고리 가져오는 로직 redis 에서 꺼내오도록 변경

--- src/main/java/store/mybooks/front/category/service/CategoryService.java
+++ src/main/java/store/mybooks/front/category/service/CategoryService.java
@@ -3,6 +3,8 @@ package store.mybooks.front.category.service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import store.mybooks.front.admin.book.model.response.BookBriefResponse;
 import store.mybooks.front.category.adaptor.CategoryAdaptor;
@@ -25,17 +27,34 @@ import store.mybooks.front.pageable.dto.response.PageResponse;
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryAdaptor categoryAdaptor;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private static final String ALL_CATEGORIES_KEY = "main:all_categories";
+
+    @Scheduled(cron = "0 0 1 * * *")
+    public void fetchAndCacheCategoryList() {
+        redisTemplate.delete(ALL_CATEGORIES_KEY);
+
+        List<CategoryGetResponseForMainView> categoryList = categoryAdaptor.getCategoriesForMainView();
+        redisTemplate.opsForValue().set(ALL_CATEGORIES_KEY, categoryList);
+    }
 
     public List<CategoryGetResponseForMainView> getCategoriesForMainView() {
-        return categoryAdaptor.getCategoriesForMainView();
+        List<CategoryGetResponseForMainView> categoryList
+                = (List<CategoryGetResponseForMainView>) redisTemplate.opsForValue().get(ALL_CATEGORIES_KEY);
+
+        if (categoryList == null) {
+            fetchAndCacheCategoryList();
+            categoryList = (List<CategoryGetResponseForMainView>) redisTemplate.opsForValue().get(ALL_CATEGORIES_KEY);
+        }
+
+        return categoryList;
     }
 
     public CategoryGetResponseForCategoryView getCategoriesForCategoryView(Integer categoryId) {
         return categoryAdaptor.getCategoriesForCategoryView(categoryId);
     }
 
-    public PageResponse<BookBriefResponse> getBooksForCategoryView(Integer categoryId,
-                                                                   Pageable pageable) {
+    public PageResponse<BookBriefResponse> getBooksForCategoryView(Integer categoryId, Pageable pageable) {
         return categoryAdaptor.getBooksForCategoryView(categoryId, pageable);
     }
 }
