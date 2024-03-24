c1fd237e5cf5ec0d205c26771a2d59d8bc8277db
minsu <zmfjtnl1@naver.com>
2024-03-24 23:18:00 +0900

    remove: 주석 제거

--- src/main/resources/templates/assets/js/payment/success.js
+++ src/main/resources/templates/assets/js/payment/success.js
@@ -1,9 +1,6 @@
-// 쿼리 파라미터 값이 결제 요청할 때 보낸 데이터와 동일한지 반드시 확인하세요.
-// 클라이언트에서 결제 금액을 조작하는 행위를 방지할 수 있습니다.
 var status;
 document.addEventListener('DOMContentLoaded', async function () {
     const urlParams = new URLSearchParams(window.location.search);
-    // 서버로 결제 승인에 필요한 결제 정보를 보내세요.
     const pay = await confirm(urlParams)
     const successTitleElement = document.getElementById("success-title");
     const orderIdElement = document.getElementById("orderId");
